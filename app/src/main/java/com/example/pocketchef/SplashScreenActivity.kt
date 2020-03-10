7ad1c898c65cef119d331e9d520a07520e72bb30
William Wallace <wwa52@uclive.ac.nz>
2020-03-10 20:26:03 +1300

    initial commit

---
+++ app/src/main/java/com/example/pocketchef/SplashScreenActivity.kt
@@ -0,0 +1,34 @@
+package com.example.pocketchef
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import java.lang.Exception
+import java.lang.Thread.sleep
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val background = object : Thread() {
+            override  fun run() {
+                try {
+                    Thread.sleep(5000)
+
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+
+}
+
+
